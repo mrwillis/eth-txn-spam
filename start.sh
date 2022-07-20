e57bc4cdc222ce31f7c148ae1a98309342d3c54f
Victor Castell <victor@victorcastell.com>
2022-07-20 13:12:15 +0200

    No need for network host

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ trap "pkill -P $$" SIGINT SIGTERM EXIT
 PARALLELISM=$1
 RANGE=$2
 MODE=$3
-CONFIG_DATA_JSON=$(<~/borv3/config.json)
+CONFIG_DATA_JSON=$(<./config.json)
 
 if [[ -f "./mnemonics" ]]; then
     readarray -t mnemonics < ./mnemonics
@@ -22,7 +22,7 @@ for MNEMONIC in "${mnemonics[@]}"
 do
     echo Starting run script with \"$MNEMONIC\" $COUNT
     # does not work --add-host=host.docker.internal:host-gateway
-    docker run --net=host --detach -e RANGE=$RANGE -e MNEMONIC="$MNEMONIC" -e MODE="$MODE" -e CONFIG_DATA_JSON="$CONFIG_DATA_JSON" txn-spam:latest
+    docker run --detach -e RANGE=$RANGE -e MNEMONIC="$MNEMONIC" -e MODE="$MODE" -e CONFIG_DATA_JSON="$CONFIG_DATA_JSON" txn-spam:latest
     ((COUNT=COUNT+1))
     if [ "$COUNT" -ge "$PARALLELISM" ]; then
         break;
